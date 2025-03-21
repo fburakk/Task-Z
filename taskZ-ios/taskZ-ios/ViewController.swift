c1b9f772e1c97e67e24cfcecfb72a7c54b070b1a
Burak Köse <iosdeveloper19@hotmail.com>
2025-03-22 01:36:42 +0300

    Add initial iOS project

---
+++ taskZ-ios/taskZ-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskZ-ios
+//
+//  Created by Burak Köse on 22.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ taskZ-ios/taskZ-iosTests/taskZ_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  taskZ_iosTests.swift
+//  taskZ-iosTests
+//
+//  Created by Burak Köse on 22.03.2025.
+//
+
+import Testing
+@testable import taskZ_ios
+
+struct taskZ_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
