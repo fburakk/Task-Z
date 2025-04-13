eabea0579d3dc95ab36cf2ad71b2f4b4568ae83a
yigitzip <128749833+yigitzip@users.noreply.github.com>
2025-04-13 22:59:44 +0300

    feat:login page

--- taskZ-web/src/app/app.component.ts
+++ taskZ-web/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';  // Import NavbarComponent
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,  // Optional: If using Angular standalone components (Angular 14+)
-  imports: [NavbarComponent],  // Make sure NavbarComponent is included in imports
+  imports: [NavbarComponent,RouterOutlet],  // Make sure NavbarComponent is included in imports
 })
 export class AppComponent {
   title = 'taskz-web';
--- taskZ-web/src/app/app.routes.ts
+++ taskZ-web/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path:'register',
+        component:RegisterComponent
+    },
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    }
+];
