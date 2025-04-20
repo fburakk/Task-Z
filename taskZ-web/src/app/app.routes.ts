d4cec9af86940600495d39aa1fc0a556ba6e5d66
yigitzip <128749833+yigitzip@users.noreply.github.com>
2025-04-20 23:55:41 +0300

    navbar update

--- taskZ-web/src/app/app.routes.ts
+++ taskZ-web/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './auth.guard';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
     {
@@ -22,5 +23,10 @@ export const routes: Routes = [
     {
         path:'login',
         component:LoginComponent
+    },
+    {
+        path:'projects',
+        component:ProjectsComponent,
+        canActivate: [authGuard]
     }
 ];
