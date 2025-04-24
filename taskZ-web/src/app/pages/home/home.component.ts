ad452dd94e42fd1f248188ae4a269b9bdf5b5b7a
yigitzip <128749833+yigitzip@users.noreply.github.com>
2025-04-24 15:23:03 +0300

    add board

--- taskZ-web/src/app/pages/home/home.component.ts
+++ taskZ-web/src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { Workspace, WorkspaceService } from '../../core/services/workspace.servi
 import { CommonModule } from '@angular/common';
 import { Board, BoardService } from '../../core/services/board.service';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
@@ -21,11 +22,36 @@ export class HomeComponent implements OnInit {
     private boardService: BoardService
   ) {}
 
+  showModal: boolean = false;
+newBoardName: string = '';
+selectedWorkspaceId: number | null = null;
+
+openModal(workspaceId: number) {
+  this.selectedWorkspaceId = workspaceId;
+  this.newBoardName = '';
+  this.showModal = true;
+}
+
+closeModal() {
+  this.showModal = false;
+}
+
+createBoard() {
+  if (!this.newBoardName.trim() || this.selectedWorkspaceId === null) return;
+
+  this.boardService.createBoard(this.selectedWorkspaceId, this.newBoardName, '#FFFFFF').subscribe(newBoard => {
+    if (!this.boardsByWorkspace[this.selectedWorkspaceId!]) {
+      this.boardsByWorkspace[this.selectedWorkspaceId!] = [];
+    }
+    this.boardsByWorkspace[this.selectedWorkspaceId!].push(newBoard);
+    this.closeModal();
+  });
+}
+
   ngOnInit(): void {
     this.workspaceService.getWorkspaces().subscribe((workspaces) => {
       this.workspaces = workspaces;
 
-      // Her workspace için boardları çek
       this.workspaces.forEach((workspace) => {
         this.boardService.getBoards(workspace.id).subscribe((boards) => {
           this.boardsByWorkspace[workspace.id] = boards;
